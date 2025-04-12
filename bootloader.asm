908fd1367103c78f4b9f0847b5bdbf0a3cf1996d
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2025-04-12 18:27:17 +0200

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,28 +1,31 @@
-; bootloader.asm
-[org 0x7c00]
+[BITS 16]
+[ORG 0x7C00]
 
 start:
-    cli                   ; Désactiver les interruptions
-    mov ax, 0x07C0        ; Charger l'adresse du bootloader en mémoire
-    add ax, 288           ; Déplacer le pointeur de pile
-    mov ss, ax
-    mov sp, 4096
+	mov ah, 0x0                ; Effacer l'écran
+	mov al, 3
+	int 0x10
 
-    ; Afficher un caractère à l'écran => tester le bootloader
-    mov ah, 0x0E          ; Appel le service vidéo BIOS pour afficher un caractère
-    mov al, 'B'           ; Caractère à afficher
-    int 0x10              ; Fermer le BIOS
+	mov bx, HELLO_MSG          ; Mettre l'adresse de HELLO_MSG dans le registre BX
+	call print_string          ; Appele de la fonction pour afficher la chaine de caractères
 
-    ; Charger le noyau (1 secteur) à partir du disque
-    mov bx, 0x1000        ; Charger le noyau à l'adresse => 0x1000
-    mov ah, 0x02          ; Fonction BIOS => lire un secteur
-    mov al, 1             ; Lire 1 secteur
-    mov ch, 0             
-    mov dh, 0             
-    mov cl, 2             ; Secteur 2 => car le secteur 1 contient le bootloader
-    int 0x13              ; Appel BIOS : lecture du disque
+hang:
+	jmp hang                   ; Boucle infie
 
-    jmp 0x1000:0000       ; Sauter à l'adresse du noyau
+times 510-($-$$) db 0          ; Remplir jusqu'à 510 octets
+dw 0xAA55                      ; Signature du boot
 
-times 510 - ($-$$) db 0   ; Remplir le bootloader jusqu'a 510 octets
-dw 0xAA55                 ; Signature du bootloader
+HELLO_MSG: 
+	db 'Hello, World!', 0      ; Chaine de caractères
+
+print_string:
+	mov al, [bx]
+	cmp al, 0
+	je done
+	mov ah, 0x0E
+	int 0x10
+	inc bx
+	jmp print_string           ; Afficher le prochain caractères
+
+done:
+	ret

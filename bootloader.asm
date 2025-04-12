181522fffd1b69dc15235584edd904bc74784da3
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2025-04-12 19:07:10 +0200

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -2,30 +2,24 @@
 [ORG 0x7C00]
 
 start:
-	mov ah, 0x0                ; Effacer l'écran
-	mov al, 3
-	int 0x10
+    mov ah, 0x0     ; Changer le mode vidéo (efface l'écran)
+    mov al, 0x03
+    int 0x10
 
-	mov bx, HELLO_MSG          ; Mettre l'adresse de HELLO_MSG dans le registre BX
-	call print_string          ; Appele de la fonction pour afficher la chaine de caractères
+    mov si, HELLO_MSG
+.print:
+    lodsb
+    cmp al, 0
+    je .done
+    mov ah, 0x0E
+    int 0x10
+    jmp .print
 
-hang:
-	jmp hang                   ; Boucle infie
+.done:
+    jmp $
 
-times 510-($-$$) db 0          ; Remplir jusqu'à 510 octets
-dw 0xAA55                      ; Signature du boot
+HELLO_MSG:
+    db "Bienvenue sur MyOS V1 !", 0
 
-HELLO_MSG: 
-	db 'Hello, World!', 0      ; Chaine de caractères
-
-print_string:
-	mov al, [bx]
-	cmp al, 0
-	je done
-	mov ah, 0x0E
-	int 0x10
-	inc bx
-	jmp print_string           ; Afficher le prochain caractères
-
-done:
-	ret
+times 510-($-$$) db 0
+dw 0xAA55
